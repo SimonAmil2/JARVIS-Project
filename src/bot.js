0be488715268f5d802ec6e7e6c411a30e75d3896
Simon AMILCARO <simonamilcaro@MacBook-Pro-de-Simon.local>
2020-11-11 15:35:23 +0100

    New User Event : Testing

--- src/bot.js
+++ src/bot.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 
-const { Client } = require('discord.js');
+const { Client} = require('discord.js');
 
 const client = new Client();
 const PREFIX = '$';
+
 
 client.on('ready', () => {
     console.log(`${client.user.username} has logged in.`);
@@ -11,10 +12,10 @@ client.on('ready', () => {
 
 client.on('message', (message) => {
     if (message.author.bot) return;
-    // console.log(`[${message.author.tag}] : ${message.content}`);
-    // if (message.content === 'hello') {
-    //     message.reply('Hello there!');
-    // }
+    console.log(`[${message.author.tag}] : ${message.content}`);
+    if (message.content === 'hello') {
+    message.reply('Hello there!');
+    }
 
     if (message.content.startsWith(PREFIX)) {
         const [CMD_NAME, ...args] = message.content
@@ -25,6 +26,22 @@ client.on('message', (message) => {
         console.log(args);
     }
 
+});
+
+
+
+client.on('guildMemberAdd', member => {
+    console.log('New User join event');
+	const channel = member.guild.channels.cache.find(ch => ch.name === 'dgeneraru');
+	if (!channel) return;
+
+	channel.send(`Welcome to the server, ${member}!`);
+});
+
+client.on('message', message => {
+	if (message.content === 'join') {
+		client.emit('guildMemberAdd', message.member);
+	}
 });
 
 client.on('error', (error) => {
